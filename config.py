76fc0ed872a81483cce550de5a3852794947713c
CharlieDBest <carlosmoralesleon2006@gmail.com>
2026-02-05 00:58:54 +0100

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    DEBUG=True
-    SQLALCHEMY_DATABASE_URI ='mysql+pymysql://Charlie4ever:michus123@Charlie4ever.mysql.eu.pythonanywhere-services.com/Charlie4ever$flaskydb'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'Clave secreta durisima'
